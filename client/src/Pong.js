0341a8887f2fa05102c6511edd04fb09e6e8f5aa
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-06-06 17:40:33 +0200

    Proxy requests from client to Express

---
+++ client/src/Pong.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Pong extends React.Component {
+  componentDidMount() {
+    fetch('/ping')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(pong => {
+        console.log(pong);
+      });
+  }
+
+  render() {
+    return(
+      <div>
+        Greetings from Pong component!
+      </div>
+    )
+  }
+}
+
+export default Pong
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => res.send("Hello, World!"));
+app.get('/ping', (req, res) => res.json({ pong: "Pong!"}))
 
-const port = process.env.PORT || 3000
+
+const port = process.env.PORT || 3100
 app.listen(port, () => {
   console.log(`Express listening on port ${port}`)
 })
