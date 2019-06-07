dd5d6b460bcfb485c801fab4e79f911aeb321c35
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-06-07 13:54:15 +0200

    Add Websocket communication

--- client/src/Pong.js
+++ client/src/Pong.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react";
 
 class Pong extends React.Component {
+  ws = new window.WebSocket(
+    `ws://localhost:${process.env.REACT_APP_WEBSOCKET_PORT}`
+  );
+
   componentDidMount() {
-    fetch('/ping')
+    fetch("/ping")
       .then(response => {
-        console.log(response)
-        return response.json()
+        console.log(response);
+        return response.json();
       })
       .then(pong => {
         console.log(pong);
       });
+
+    this.ws.onopen = () => {
+      console.log("Websocket connected");
+    };
+
+    this.ws.onmessage = event => {
+      const message = JSON.parse(event.data);
+      console.log("Websocket received message:", message);
+    };
   }
 
   render() {
-    return(
-      <div>
-        Greetings from Pong component!
-      </div>
-    )
+    return <div>Greetings from Pong component!</div>;
   }
 }
 
-export default Pong
+export default Pong;
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require("express")
-const app = express()
+const port = process.env.PORT || 3100;
 
-app.get('/', (req, res) => res.send("Hello, World!"));
-app.get('/ping', (req, res) => res.json({ pong: "Pong!"}))
+// Express
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Hello, World!"));
+app.get("/ping", (req, res) => res.json({ pong: "Pong!" }));
+const expressServer = app.listen(port, () => {
+  console.log(`Express listening on port ${port}`);
+});
 
+// Websocket
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server: expressServer });
+console.log(`Websocket listening on port ${port}`);
+wss.on("connection", function connection(ws) {
+  console.log(`Websocket client connected`);
+  ws.send(JSON.stringify({ status: "connected" }));
 
-const port = process.env.PORT || 3100
-app.listen(port, () => {
-  console.log(`Express listening on port ${port}`)
-})
+  ws.on("message", function incoming(event) {
+    console.log(`Websocket client sent messaget ${event}`);
+    const message = JSON.parse(event);
+  });
+
+  ws.on("close", function incoming() {
+    console.log(`Websocket client disconnected`);
+  });
+});
