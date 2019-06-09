c078af6bd895bd0c55b9e6b6a5f8db1f68f7a8b1
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-06-09 14:36:45 +0200

    Register client actions

--- client/src/Game.js
+++ client/src/Game.js
@@ -9,11 +9,22 @@ class Game extends React.Component {
     this.ws.onopen = () => {
       console.log("Websocket connected");
     };
-
     this.ws.onmessage = event => {
       const message = JSON.parse(event.data);
       console.log("Websocket received message:", message);
     };
+
+    document.addEventListener("keydown", event => {
+      if (event.keyCode === 87) {
+        this.ws.send(JSON.stringify({ action: "up" }));
+      } else if (event.keyCode === 83) {
+        this.ws.send(JSON.stringify({ action: "down" }));
+      } else if (event.keyCode === 65) {
+        this.ws.send(JSON.stringify({ action: "left" }));
+      } else if (event.keyCode === 68) {
+        this.ws.send(JSON.stringify({ action: "right" }));
+      }
+    });
   }
 
   render() {
