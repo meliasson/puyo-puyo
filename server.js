b324e3dc3d1ad62431a20b5d8d14669d9d5ea419
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-06-09 13:48:32 +0200

    Introduce concept of game instance on server

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 const port = process.env.PORT || 3100;
 
+// Game
+const game = {
+  clients: {},
+  join: function(clientId) {
+    this.clients[clientId] = {
+      id: clientId,
+      position: [Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)]
+    };
+  },
+  leave: function(clientId) {
+    delete this.clients[clientId];
+  },
+  update: function(clientId, action) {
+    const client = this.clients[clientId];
+    switch (action) {
+      case "up":
+        client.position[1] = client.position[1] - 1;
+        break;
+      case "down":
+        client.position[1] = client.position[1] + 1;
+        break;
+      case "left":
+        client.position[0] = client.position[0] - 1;
+        break;
+      case "right":
+        client.position[0] = client.position[0] + 1;
+        break;
+      default:
+        throw new Error("Received unknown action");
+    }
+  }
+};
+
 // Express
 const express = require("express");
 const path = require("path");
@@ -21,11 +54,13 @@ console.log(`Websocket listening on port ${port}`);
 wss.on("connection", function connection(ws) {
   ws.id = uuidv1();
   console.log(`Websocket client ${ws.id} connected`);
+  game.join(ws.id);
   ws.send(JSON.stringify({ status: "connected", id: ws.id }));
 
   ws.on("message", function incoming(event) {
-    console.log(`Websocket client ${ws.id} sent message ${event}`);
+    console.log(`Websocket received message ${event} from client ${ws.id}`);
     const message = JSON.parse(event);
+    game.update(ws.id, message.action);
   });
 
   ws.on("close", function incoming() {
