6bf9933b10e4284cc19e26635f9e247f2e703b18
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-06-09 14:00:16 +0200

    Render game component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Game from "./Game.js";
 import Pong from "./Pong.js";
 
 function App() {
   return (
     <div>
       <Pong />
+      <Game />
     </div>
   );
 }
