8e2199d3f1bb187fb46dff17a62ec0b612e9e529
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-07-16 14:10:43 +0200

    Move server files into own directory

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/client/build/index.html"))
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
 app.get("/ping", (req, res) => res.json({ pong: "Pong!" }));
 
